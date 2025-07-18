40e67664578cdca3db24d4bb2211e444d08d6351
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-18 17:26:21 +0200

    move beckend to folder

---
+++ back-end/server.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import express from 'express';
+import connectDB from './config/mongodb.js';
+import cors from 'cors';
+import userRouter from './routes/userRoute.js';
+import connectCloudinary from './config/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import orderRouter from './routes/orderRoute.js';
+import cartRouter from './routes/cartRoute.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/order', orderRouter);
+app.use('/api/cart', cartRouter);
+
+app.listen(port, () => {
+  console.log('Server started on PORT : ' + port);
+});
