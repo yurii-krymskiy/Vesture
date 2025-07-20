eac31ef280442c53e0a721088c531a3233e6b527
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-20 23:29:12 +0200

    add vercel.json

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ import orderRouter from './routes/orderRoute.js';
 import cartRouter from './routes/cartRoute.js';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 connectDB();
 connectCloudinary();
