bff6521d77942d98c4f0ae657c72109947a939ec
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-20 16:35:47 +0200

    remove razorpay

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -1,7 +1,6 @@
 import orderModel from '../models/orderModel.js';
 import userModel from '../models/userModel.js';
 import Stripe from 'stripe';
-import razorpay from 'razorpay';
 
 // global variables
 const currency = 'inr';
@@ -9,11 +8,6 @@ const deliveryCharge = 10;
 
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-const razorpayInstance = new razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
 
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
@@ -116,61 +110,6 @@ const verifyStripe = async (req, res) => {
   }
 };
 
-// Placing orders using Razorpay Method
-const placeOrderRazorpay = async (req, res) => {
-  try {
-    const { userId, items, amount, address } = req.body;
-
-    const orderData = {
-      userId,
-      items,
-      address,
-      amount,
-      paymentMethod: 'Razorpay',
-      payment: false,
-      date: Date.now(),
-    };
-
-    const newOrder = new orderModel(orderData);
-    await newOrder.save();
-
-    const options = {
-      amount: amount * 100,
-      currency: currency.toUpperCase(),
-      receipt: newOrder._id.toString(),
-    };
-
-    await razorpayInstance.orders.create(options, (error, order) => {
-      if (error) {
-        console.log(error);
-        return res.json({ success: false, message: error });
-      }
-      res.json({ success: true, order });
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: error.message });
-  }
-};
-
-const verifyRazorpay = async (req, res) => {
-  try {
-    const { userId, razorpay_order_id } = req.body;
-
-    const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
-    if (orderInfo.status === 'paid') {
-      await orderModel.findByIdAndUpdate(orderInfo.receipt, { payment: true });
-      await userModel.findByIdAndUpdate(userId, { cartData: {} });
-      res.json({ success: true, message: 'Payment Successful' });
-    } else {
-      res.json({ success: false, message: 'Payment Failed' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: error.message });
-  }
-};
-
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
   try {
@@ -208,13 +147,4 @@ const updateStatus = async (req, res) => {
   }
 };
 
-export {
-  verifyRazorpay,
-  verifyStripe,
-  placeOrder,
-  placeOrderStripe,
-  placeOrderRazorpay,
-  allOrders,
-  userOrders,
-  updateStatus,
-};
+export { verifyStripe, placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus };
--- back-end/routes/orderRoute.js
+++ back-end/routes/orderRoute.js
@@ -2,12 +2,10 @@ import express from 'express';
 import {
   placeOrder,
   placeOrderStripe,
-  placeOrderRazorpay,
   allOrders,
   userOrders,
   updateStatus,
   verifyStripe,
-  verifyRazorpay,
 } from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js';
@@ -21,13 +19,11 @@ orderRouter.post('/status', adminAuth, updateStatus);
 // Payment Features
 orderRouter.post('/place', authUser, placeOrder);
 orderRouter.post('/stripe', authUser, placeOrderStripe);
-orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
 
 // User Feature
 orderRouter.post('/userorders', authUser, userOrders);
 
 // verify payment
 orderRouter.post('/verifyStripe', authUser, verifyStripe);
-orderRouter.post('/verifyRazorpay', authUser, verifyRazorpay);
 
 export default orderRouter;
