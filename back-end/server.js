3fc72b1c066efe4d2e173040c004b87ce4a77942
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-20 18:22:52 +0200

    updte port

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ import orderRouter from './routes/orderRoute.js';
 import cartRouter from './routes/cartRoute.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 connectDB();
 connectCloudinary();
