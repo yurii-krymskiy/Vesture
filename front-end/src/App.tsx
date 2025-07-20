b8974cefcd88bcc65ab10b3ef5555edde0f980a5
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-20 16:19:24 +0200

    add cart, login pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,6 +8,8 @@ import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
 
 function App() {
 
@@ -22,8 +24,8 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
-        {/* <Route path='/cart' element={<Cart />} /> */}
-        {/* <Route path='/login' element={<Login />} /> */}
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
         {/* <Route path='/place-order' element={<PlaceOrder />} /> */}
         {/* <Route path='/orders' element={<Orders />} /> */}
         {/* <Route path='/verify' element={<Verify />} /> */}
