088589dda4ac2562d02c37973cd76098b5d9b92a
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-20 15:52:56 +0200

    Contact, Product pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,6 +6,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
 
 function App() {
 
@@ -18,8 +20,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
-        {/* <Route path='/contact' element={<Contact />} /> */}
-        {/* <Route path='/product/:productId' element={<Product />} /> */}
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
         {/* <Route path='/cart' element={<Cart />} /> */}
         {/* <Route path='/login' element={<Login />} /> */}
         {/* <Route path='/place-order' element={<PlaceOrder />} /> */}
